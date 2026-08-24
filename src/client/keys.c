6f1fbb386e4c951f74803a95c154dac6ccd7929c
DrUm78 <cruau.david@gmail.com>
2026-08-24 19:19:29 +0200

    Only the 'Start' button brings the menu up during attract mode now

--- src/client/keys.c
+++ src/client/keys.c
@@ -837,9 +837,9 @@ void Key_Event (int key, qboolean down, unsigned time)
 	}
 
 	// any key during the attract mode will bring up the menu
-	if (cl.attractloop && cls.key_dest != key_menu &&
+	/*if (cl.attractloop && cls.key_dest != key_menu &&
 		!(key >= K_F1 && key <= K_F12))
-		key = K_ENTER;
+		key = K_ENTER;*/
 
 	// menu key is hardcoded, so the user can never unbind it
 	if (key == K_ENTER)
