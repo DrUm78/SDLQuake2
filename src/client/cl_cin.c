0a159bfea770cce19cf82b44d5f924bc68446c99
DrUm78 <cruau.david@gmail.com>
2026-09-01 22:45:28 +0200

    [Cinematics] Fixed sound stuttering over time when cinematics play ('CL_Snd_Restart_f ()' was called at each cinematic start/stop)

--- src/client/cl_cin.c
+++ src/client/cl_cin.c
@@ -27,7 +27,6 @@ typedef struct
 
 typedef struct
 {
-	qboolean	restart_sound;
 	int		s_rate;
 	int		s_width;
 	int		s_channels;
@@ -178,14 +177,6 @@ void SCR_StopCinematic (void)
 		Z_Free (cin.hnodes1);
 		cin.hnodes1 = NULL;
 	}
-
-	// switch back down to 11 khz sound if necessary
-	if (cin.restart_sound)
-	{
-		cin.restart_sound = false;
-		CL_Snd_Restart_f ();
-	}
-
 }
 
 /*
@@ -578,7 +569,6 @@ void SCR_PlayCinematic (char *arg)
 	int		width, height;
 	byte	*palette;
 	char	name[MAX_OSPATH], *dot;
-	int		old_khz;
 
 	// make sure CD isn't playing music
 	CDAudio_Stop();
@@ -633,16 +623,6 @@ void SCR_PlayCinematic (char *arg)
 	cin.s_channels = LittleLong(cin.s_channels);
 
 	Huff1TableInit ();
-
-	// switch up to 22 khz sound if necessary
-	old_khz = Cvar_VariableValue ("s_khz");
-	if (old_khz != cin.s_rate/1000)
-	{
-		cin.restart_sound = true;
-		Cvar_SetValue ("s_khz", cin.s_rate/1000);
-		CL_Snd_Restart_f ();
-		Cvar_SetValue ("s_khz", old_khz);
-	}
 
 	cl.cinematicframe = 0;
 	cin.pic = SCR_ReadNextFrame ();
